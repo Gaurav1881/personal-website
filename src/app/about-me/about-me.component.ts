84079d4317e23fa267ac72a540fe2b6be458d5f5
Gaurav Sharma <gaurav.sharma@caseware.com>
2019-12-16 16:35:27 -0500

    added quiz panel

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  questions: string[][] = [
+    ['Who is my favorite basketball player?', 'Lebron James' , 'Fred Vanvleet', 'Pascal Siakam', 'Kawhi Leonard', 'Fred Vanvleet'],
+    ['What kind of dog do I have?', 'Pitbull' , 'Golden Doodle', 'Beagle', 'Cockerspaniel & Poodle Mix', 'Cockerspaniel & Poodle Mix'],
+    ['Which of the following is NOT one of my hobbies?', 'Cooking' , 'Archery', 'Going to the Gym', 'Drawing', 'Archery']
+  ];
+
+  questionIndex = 0;
+
+
+  selectedAnswer = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelectButton(button: HTMLButtonElement) {
+    this.selectedAnswer = button.textContent;
+  }
+
+  onSelectNext() {
+    this.questionIndex = this.questionIndex + 1;
+    if (this.questionIndex == this.questions.length) {
+      this.questionIndex = 0;
+    }
+    this.selectedAnswer = '';
+  }
+
+  getClass(option) {
+    let curOption = this.questions[this.questionIndex][option];
+    if (this.selectedAnswer == curOption) {
+      if (curOption == this.questions[this.questionIndex][5]) {
+        return "btn btn-success";
+      }
+      return "btn btn-danger";
+    } else {
+      return "btn btn-light";
+    }
+  }
 }
