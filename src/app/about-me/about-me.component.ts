c7daf768a24ec0834b4d0b89a55d21d0fbbb6154
Gaurav1881 <39128612+Gaurav1881@users.noreply.github.com>
2019-12-16 23:28:31 -0500

    added more questions

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -8,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AboutMeComponent implements OnInit {
 
   questions: string[][] = [
-    ['Who is my favorite basketball player?', 'Lebron James' , 'Fred Vanvleet', 'Pascal Siakam', 'Kawhi Leonard', 'Fred Vanvleet'],
-    ['What kind of dog do I have?', 'Pitbull' , 'Golden Doodle', 'Beagle', 'Cockerspaniel & Poodle Mix', 'Cockerspaniel & Poodle Mix'],
-    ['Which of the following is NOT one of my hobbies?', 'Cooking' , 'Archery', 'Going to the Gym', 'Drawing', 'Archery']
+    ['Who is my favorite basketball player?', 'Lebron James', 'Fred Vanvleet', 'Pascal Siakam', 'Kawhi Leonard', 'Fred Vanvleet'],
+    ['What kind of dog do I have?', 'Pitbull', 'Golden Doodle', 'Beagle', 'Cockerspaniel & Poodle Mix', 'Cockerspaniel & Poodle Mix'],
+    ['Which of the following is NOT one of my hobbies?', 'Cooking', 'Archery', 'Going to the Gym', 'Drawing', 'Archery'],
+    ['What is my ideal weekend activity to do with friends?', 'An Escape Room', 'Watch a Movie', 'Go Shopping', 'Play Tennis', 'An Escape Room'],
+    ['Which of the following programming languages have I NOT worked with?', 'Java', 'Python', 'C++', 'Ruby', 'Ruby'],
+    ['What is my favorite game category?', 'Adventure Games', 'Puzzle/Strategy Games', 'FPS Games', 'Simulation Games', 'Puzzle/Strategy Games']
   ];
 
   questionIndex = 0;
